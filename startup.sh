7edeac49f3ec6216f4705529107e4f8369bc39d4
Oliver C. Lanz <oliver.lanz@gigglingbits.com>
2025-03-11 18:55:15 +0100

    Fix startup script to patch Gradio launch function for public network access

--- startup.sh
+++ startup.sh
@@ -49,12 +49,12 @@ mkdir -p "$REPO_HOME/output/gradio"
 ln -sfn "${OUTPUT_HOME}" "$REPO_HOME/output/gradio"
 
 # patching the start-up function, so that the script listens on the public network interface
-if grep -q 'server_host="0.0.0.0", server_port=args.port, allowed_paths=[output_dir])' "$REPO_HOME/gradio_demo.py"; then
+if grep -q 'server_name="0.0.0.0", server_port=args.port, allowed_paths=[output_dir])' "$REPO_HOME/gradio_demo.py"; then
     echo "Launch function is already patched."
 else
     # Replace demo.launch() with demo.launch(server_name="0.0.0.0", server_port=args.port)
     # This causes the Gradio server to listen on all network interfaces
-    sed -i 's/server_port=args.port, allowed_paths=[output_dir])/server_host="0.0.0.0", server_port=args.port, allowed_paths=[output_dir])/g' "$REPO_HOME/gradio_demo.py"
+    sed -i 's/server_port=args.port, allowed_paths=\[output_dir\])/server_name="0.0.0.0", server_port=args.port, allowed_paths=[output_dir])/g' "$REPO_HOME/gradio_demo.py"
     echo "Launch function patched with server_name=\"0.0.0.0\", server_port=args.port."
     NEEDS_INSTALL=1
 fi
